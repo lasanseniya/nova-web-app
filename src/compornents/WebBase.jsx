8e4481db5b3bf92ff533d513b424d4818feaeb37
Chamath Anjula <124298134+chamathAn@users.noreply.github.com>
2024-02-09 16:26:37 +0530

    folder structure and small peice of base window

---
+++ src/compornents/WebBase.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../compornent-css/WebBase.css";
+
+const WebBase = () => {
+  return (
+    <div>
+      <div className="right-panel-container">
+        <div className="left-panel-container"></div>
+      </div>
+    </div>
+  );
+};
+
+export default WebBase;
