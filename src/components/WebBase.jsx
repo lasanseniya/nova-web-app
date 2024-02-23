12e3ef5580e7b51acfa6ca98378b75652b840cd1
lasanseniya <lasanseniya@gmail.com>
2024-02-23 23:51:16 +0530

    removed redundant css and files

--- src/components/WebBase.jsx
+++ src/components/WebBase.jsx
@@ -1,23 +1,9 @@
 import { useState } from "react";
-import logo from "../assets/logo.png";
-import user from "../assets/user.png";
-import "../component-css/WebBase.css";
 import Sidebar from "./Sidebar";
 import NavBar from "./NavBar";
 
 const WebBase = () => {
-  const [newNoteButnClicked, setNewNoteButnClicked] = useState(false);
-  const [myNotesButnClicked, setMyNotesButnClicked] = useState(false);
   const [userName, setUserName] = useState("<user name>");
-
-  function handleNewNoteClick() {
-    setNewNoteButnClicked(true);
-  }
-
-  function handleMyNotesClick() {
-    setMyNotesButnClicked(true);
-    setNewNoteButnClicked(false);
-  }
 
   return (
     <>
