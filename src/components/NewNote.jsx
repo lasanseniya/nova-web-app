bdfe28b1fd795abf16b236735dbba625908beff3
lasanseniya <lasanseniya@gmail.com>
2024-02-24 10:37:13 +0530

    Added prettier code formatter dependency

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,7 +1,7 @@
 function NewNote() {
   return (
     <div className="sm:ml-64">
-      <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+      <div className="mt-14 rounded-lg border-2 border-dashed border-gray-200 p-4 dark:border-gray-700">
         New note area
       </div>
     </div>
