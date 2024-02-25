882e957b293af03ef7b7b2e393ab5f2f72220546
lasanseniya <lasanseniya@gmail.com>
2024-02-25 17:26:27 +0530

    Added the content areas

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-
 function NewNote() {
   return (
-    <div
-      className="absolute border-2 bg-[#f3f3f3] border-gray-200 border-dashed rounded-lg dark:border-gray-700 top-20 sm:left-64
-      p-20"
-    >
-      New note area
+    <div className=" sm:ml-64">
+      {/* New note container */}
+      <div className=" mt-20 h-[calc(100vh-5rem)] border-2 border-dashed border-gray-200 bg-gradient-to-b from-slate-900 via-slate-900 to-sky-950 p-4 text-white dark:border-gray-700">
+        New note area
+        {/* Content area */}
+      </div>
     </div>
   );
 }
