dfc4f3ac373745e74636182630526a85334f133f
lasanseniya <lasanseniya@gmail.com>
2024-02-26 21:32:37 +0530

    Added a div container to new notes.jsx

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -2,9 +2,11 @@ function NewNote() {
   return (
     <div className=" sm:ml-64">
       {/* New note container */}
-      <div className=" mt-20 h-[calc(100vh-5rem)] border-2 border-dashed border-gray-200 bg-gradient-to-b from-slate-900 via-slate-900 to-sky-950 p-4 text-white dark:border-gray-700">
-        New note area
-        {/* Content area */}
+      <div className="mt-20 h-[calc(100vh-5rem)] bg-gradient-to-b from-slate-900 via-slate-900 to-sky-950 p-4 text-white">
+        {/* Container */}
+        <div className="flex h-full place-content-center border-2 border-dashed border-blue-300">
+          {/*Youtube link input box*/}
+        </div>
       </div>
     </div>
   );
