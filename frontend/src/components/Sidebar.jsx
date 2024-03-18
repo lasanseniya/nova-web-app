0d78607f6a448420aef78e43202c302db1edee3d
lasanseniya <lasanseniya@gmail.com>
2024-03-18 18:44:13 +0530

    Fixed routing within dashboard page

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -9,20 +9,20 @@ function Sidebar() {
   return (
     <aside
       id="logo-sidebar"
-      className={`fixed top-0 z-10 left-0 w-64 h-screen transition-transform -translate-x-full pt-60 sm:translate-x-0 bg-gradient-to-b from-[rgb(1,20,42)] from-20% to-[#00387B]
+      className={`fixed left-0 top-0 z-10 h-screen w-64 -translate-x-full bg-gradient-to-b from-[rgb(1,20,42)] from-20% to-[#00387B] pt-60 transition-transform sm:translate-x-0
       ${isVisible ? "translate-x-0" : "-translate-x-full"} 
       `}
       aria-label="Sidebar"
     >
       {/* Sidebar content */}
-      <div className="h-full px-3 pb-4 overflow-y-auto">
+      <div className="h-full overflow-y-auto px-3 pb-4">
         <ul className="space-y-2 font-medium">
           <li>
             <Link
-              to="New-Note"
-              className="flex items-right p-2 mt-4 py-4 text-[#F3F3F3] rounded-l-lg hover:bg-gray-600 focus:drop-shadow-[1px_0_8px_rgba(0,132,255,0.2)]  focus:bg-[#01142A] focus:text-[#0084FF] relative left-3"
+              to=""
+              className="items-right relative left-3 mt-4 flex rounded-l-lg p-2 py-4 text-[#F3F3F3]  hover:bg-gray-600 focus:bg-[#01142A] focus:text-[#0084FF] focus:drop-shadow-[1px_0_8px_rgba(0,132,255,0.2)]"
             >
-              <div className="flex flex-row justify-between gap-5 ms-3 float-end">
+              <div className="float-end ms-3 flex flex-row justify-between gap-5">
                 {<VscAdd className="mt-1" />}
                 New Note
               </div>
@@ -30,10 +30,10 @@ function Sidebar() {
           </li>
           <li>
             <Link
-              to="My-Notes"
-              className="flex items-right p-2 mt-4 py-4 text-[#F3F3F3] rounded-l-lg hover:bg-gray-700 group focus:drop-shadow-[1px_0_8px_rgba(0,132,255,0.2)] focus:bg-[#01142A] focus:text-[#0084FF] relative left-3"
+              to="my-Notes"
+              className="items-right group relative left-3 mt-4 flex rounded-l-lg p-2 py-4 text-[#F3F3F3] hover:bg-gray-700 focus:bg-[#01142A] focus:text-[#0084FF] focus:drop-shadow-[1px_0_8px_rgba(0,132,255,0.2)]"
             >
-              <div className="flex flex-row justify-between gap-5 ms-3 float-end">
+              <div className="float-end ms-3 flex flex-row justify-between gap-5">
                 {<TfiFiles className="mt-1" />}
                 My Notes
               </div>
@@ -43,7 +43,7 @@ function Sidebar() {
       </div>
 
       <div
-        className={`absolute text-white left-56 top-1/2 bg-[#040D12] p-4 rounded-full cursor-pointer  sm:hidden
+        className={`absolute left-56 top-1/2 cursor-pointer rounded-full bg-[#040D12] p-4 text-white  sm:hidden
         ${isVisible ? "ml-0" : "ml-7"}`}
         onClick={() => {
           setIsVisible(!isVisible);
