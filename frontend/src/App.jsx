4f120ec234353068141e5656debf41cd92770ed5
SavindiSilva <savindisilva21@gmail.com>
2024-03-18 17:04:10 +0530

    fixed routes navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,31 @@
 import "./App.css";
 
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom";
+
+import Layout from "./pages/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
 import WebBase from "./components/WebBase.jsx";
+import NewNote from "./components/NewNote.jsx";
+import MyNotes from "./components/MyNotes.jsx";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout/>}>
+      <Route index element={<Home />}/>,
+      <Route path="/login" element={<LoginPage />}/>,
+      <Route path="/register" element={<RegisterPage />}/>,
+      <Route path="/dashboard" element={<WebBase />}>
+        <Route index element={<MyNotes />}/>,
+        <Route path="new" element={<NewNote />}/>,
+      </Route>
+    </Route>
+  )
+)
 
 function App() {
-  return (
-    <>
-      <WebBase />
-    </>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
