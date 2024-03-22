04edd0f20da8defcabd0ebf5bbcddd7edf51dbe2
Ranidhip <ranidhip@gmail.com>
2024-03-22 18:10:33 +0530

    Added connection string

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -2,7 +2,7 @@ const { mongoose } = require("mongoose")
 
 const connectDB = async () => {
     try {
-        const connect = await mongoose.connect()
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING)
         console.log(`MongoDB connected: ${connect.connection.host}`)
     } catch (error) {
         console.log(`Error: ${error.message}`)
