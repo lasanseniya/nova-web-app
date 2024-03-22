b2e7634f73b6d1419fa870bffe8a215ddd9f4877
SavindiSilva <savindisilva21@gmail.com>
2024-03-23 03:57:41 +0530

    modified image card with title

--- frontend/src/components/forms/ImageCard.jsx
+++ frontend/src/components/forms/ImageCard.jsx
@@ -2,9 +2,17 @@ import IMAGE from "../../assets/nova.png";
 
 function ImageCard() {
     return (
-      <div className="flex items-center justify-center h-2/3">
-        <img src={IMAGE} className="w-3/4" alt="Nova Logo" />
-      </div>
+      
+    <div className="hidden bg-[#01142A] bg-cover lg:block lg:w-1/2">
+        <div className="flex items-center justify-center h-2/3">
+          <img src={IMAGE} className="w-3/4" alt="Nova Logo" />
+        </div>
+
+      <p className="text-center text-6xl font-semibold text-white">NOVA</p>
+      <p className="text-center text-sm font-normal text-slate-400">
+        Your note generating partner
+      </p>
+    </div>
     );
   }
 
