059911844e9896abfe6a4ea9ec6350fb9726c877
lasanseniya <lasanseniya@gmail.com>
2024-03-23 00:51:47 +0530

    added dotenv

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
     <div>
       <h1>home page</h1>
+      <p></p>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
