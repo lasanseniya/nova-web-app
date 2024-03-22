35cffba95a81ee696ac1539e031215b92382ee96
lasanseniya <lasanseniya@gmail.com>
2024-03-22 18:28:34 +0530

    fixed module export

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,10 +1,12 @@
-const { mongoose } = require("mongoose")
+const { mongoose } = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING)
-        console.log(`MongoDB connected: ${connect.connection.host}`)
-    } catch (error) {
-        console.log(`Error: ${error.message}`)
-    }
-}
+  try {
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    console.log(`MongoDB connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+module.exports = connectDB;
