53f8416d03458c05de2129939233dfbe3ae4653d
Ranidhip <ranidhip@gmail.com>
2024-03-22 12:22:03 +0530

    centerd image and text placement

--- frontend/src/components/forms/ImageCard.jsx
+++ frontend/src/components/forms/ImageCard.jsx
@@ -2,7 +2,7 @@ import IMAGE from "../../assets/nova.png";
 
 function ImageCard() {
     return (
-      <div className="flex h-2/3">
+      <div className="flex items-center justify-center h-2/3">
         <img src={IMAGE} className="w-3/4" alt="Nova Logo" />
       </div>
     );
