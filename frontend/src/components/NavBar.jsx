4ce2d64f6892208b8cd67fe5abe71a87d4f47913
SavindiSilva <savindisilva21@gmail.com>
2024-03-24 18:11:36 +0530

    modified the user name display in the dashboard

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar({ userName }) {
       <div className=" h-20 bg-[rgb(1,20,42)] text-[#f3f3f3] ">
         <div className="relative top-6 grid grid-cols-2 border-b pb-7 font-main sm:top-5">
           <div className="relative ml-20 text-sm sm:text-2xl">Nova</div>
-          <div className="bg-items-center flex justify-center text-sm sm:text-2xl">
+          <div className="flex items-center justify-end pr-4 text-sm sm:text-2xl mr-14">
             Welcome, {userName} !
           </div>
         </div>
