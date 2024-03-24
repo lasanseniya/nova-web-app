7de7565bc7e7d49159a5344e0a259dd41e2219e5
lasanseniya <lasanseniya@gmail.com>
2024-03-24 22:49:26 +0530

    added note save button and title input box, along with save function to communicate with backend to handle CREATE note.

--- frontend/src/components/NoteContainer.jsx
+++ frontend/src/components/NoteContainer.jsx
@@ -1,19 +1,76 @@
 import parse from "html-react-parser";
-
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 function NoteContainer({ structuredNote = "", summary = "", questions = "" }) {
-  const structuredNoteWithStyls = parse(structuredNote);
+  const [title, setTitle] = useState(""); // State to hold the title value
+
+  // Parse the structured note, summary, and questions to display the HTML content
+  const structuredNoteWithStyles = parse(structuredNote);
   const questionsWithStyles = parse(questions);
   const summaryWithStyles = parse(summary);
+
+  // Function to handle saving the note to mongoDB
+  async function handleSaveNote() {
+    // Check if all the fields are filled with information
+    if (structuredNote !== "" && summary !== "" && questions !== "") {
+      // If the note is not empty, send a POST request to the backend to save the note
+      try {
+        const { data } = await axios.post("/api/notes", {
+          title: title,
+          structuredNote,
+          summary,
+          cueQuestions: questions,
+        });
+
+        // Display the error message on a toast
+        if (data.error) {
+          toast.error(data.error);
+        } else {
+          // Display the success message on a toast
+          toast.success(data.message);
+        }
+      } catch (error) {
+        console.log(error);
+
+        // Display a toast message if an error occurred while saving the note
+        toast.error("An error occurred while saving the note. 😵");
+      }
+    } else {
+      // Display a toast message if the note is empty
+      toast.error("Feels a little empty 😒");
+    }
+  }
+
   return (
     <>
-      <div className="flex-1 p-10 overflow-y-scroll sm:grid sm:grid-cols-3 sm:grid-rows-10 sm:gap-4">
+      <div className="mr-12 mt-3 flex items-center justify-end">
+        <input
+          type="text"
+          placeholder="Enter note title..."
+          className="mr-2 rounded-md p-1 text-black"
+          // Set the title value to the input value
+          value={title}
+          // Update the title value when the input box's value changes
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        {/* Button to save the note */}
+        <button
+          onClick={handleSaveNote}
+          className="top-1 rounded-lg bg-blue-600 px-3.5 py-1 shadow-lg hover:bg-blue-500 hover:shadow-blue-500/40"
+        >
+          Save Note
+        </button>
+      </div>
+      <div className="flex-1 overflow-y-scroll p-6 sm:grid sm:grid-cols-3 sm:grid-rows-10 sm:gap-4">
         <div
           contentEditable
           className="col-span-1 row-span-10 overflow-y-scroll rounded-md border border-dashed bg-[#F5F7F8] p-2 text-slate-900"
         >
-          <div className="text-2xl font-bold leading-loose text-center text-slate-900">
+          <div className="text-center text-2xl font-bold leading-loose text-slate-900">
             Questions
           </div>
           {questionsWithStyles}
@@ -22,16 +79,16 @@ function NoteContainer({ structuredNote = "", summary = "", questions = "" }) {
           contentEditable
           className="col-span-2 row-span-7 overflow-y-scroll rounded-md border border-dashed bg-[#F5F7F8] p-2 text-slate-900"
         >
-          <div className="text-2xl font-bold leading-loose text-center text-slate-900">
+          <div className="text-center text-2xl font-bold leading-loose text-slate-900">
             Structured Note
           </div>
-          {structuredNoteWithStyls}
+          {structuredNoteWithStyles}
         </div>
         <div
           contentEditable
           className="col-span-2 col-start-2 row-span-3 row-start-8 overflow-y-scroll rounded-md border  border-dashed bg-[#F5F7F8] p-2 text-slate-900"
         >
-          <div className="text-2xl font-bold leading-loose text-center text-slate-900">
+          <div className="text-center text-2xl font-bold leading-loose text-slate-900">
             Summary
           </div>
           {summaryWithStyles}
@@ -41,6 +98,7 @@ function NoteContainer({ structuredNote = "", summary = "", questions = "" }) {
   );
 }
 
+// Prop validation for the NoteContainer component
 NoteContainer.propTypes = {
   structuredNote: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
