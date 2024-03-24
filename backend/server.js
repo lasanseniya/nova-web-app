d0cd1dea79d573383abb35450ebc38831e6289e4
SavindiSilva <savindisilva21@gmail.com>
2024-03-24 17:49:11 +0530

    setup routes for crud operations

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,11 @@ const cookieParser = require("cookie-parser");
  *
  * 1. gptRouter: Router for handling GPT-3 API requests
  * 2. authRouter: Router for handling user authentication requests
+ * 3. noteRouter: Router for handling CRUD operations on notes
  */
 const { router: gptRouter } = require("./routes/router");
 const authRoutes = require("./routes/authRoutes");
+const noteRoutes = require("./routes/noteRoutes");
 
 // Establish connection to MongoDB database
 const connectDB = require("./config/dbConnection");
@@ -35,18 +37,22 @@ app.use(cookieParser());
 // Define different instances of express.Router() for different route sets
 const gptRouterInstance = express.Router();
 const authRouterInstance = express.Router();
+const noteRouterInstance = express.Router();
 
 // Apply middleware configs to different router instances
 gptRouterInstance.use(express.urlencoded({ extended: true }));
 authRouterInstance.use(express.urlencoded({ extended: false }));
+noteRouterInstance.use(express.urlencoded({ extended: false }));
 
 // Mount routers
 app.use("/api", gptRouterInstance);
 app.use("/", authRouterInstance);
+app.use("/api/notes", noteRouterInstance);
 
 // Mount route handlers to respective routers
 gptRouterInstance.use("/", gptRouter);
 authRouterInstance.use("/", authRoutes);
+noteRouterInstance.use("/", noteRoutes);
 
 // Define port number for server to listen on
 const PORT = process.env.PORT || 3001;
