dc529b6cbb92fe1cf43b0395346885e243d2d8e6
Ranidhip <ranidhip@gmail.com>
2024-04-01 06:35:49 +0530

    modified the password reset page with responsive

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -61,45 +61,46 @@ function ResetPassword() {
   };
 
   return (  
-    <div className="flex py-16 bg-gradient-to-b from-slate-900 via-indigo-900 to-slate-900 h-screen justify-center items-center">
-      <div className="flex flex-col justify-center items-center bg-gray-400 rounded-lg w-full max-w-[600px] md:w-[450px] h-[500px] p-8">
-        <div className="bg-blue-400 text-sm text-black w-[220px] h-[50px] rounded-md mt-[100px] mr-[150px]">
-          Please enter your new <br/> password below and confirm
-        </div>
-        <ImageCard />
+    <div className="flex py-8 md:py-16 bg-gradient-to-b from-slate-900 via-indigo-900 to-slate-900 h-screen justify-center items-center">
+  <div className="flex flex-col justify-center items-center bg-gray-400 rounded-lg w-full max-w-[600px] md:w-[450px] h-auto md:h-[500px] p-4 md:p-8">
+    <div className="bg-blue-400 text-sm text-black w-[220px] md:w-[200px] h-[50px] md:h-auto rounded-md mt-[50px] md:mt-[100px] mr-0 md:mr-[150px] text-center">
+      Please enter your new <br/> password below and confirm
+    </div>
+    <ImageCard />
 
-        <form className="flex flex-col items-center text-sm w-[300px] mb-[100px]" onSubmit={handlePasswordReset}>
-        <InputBox
-          className="bg-white text-gray-900 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 w-full mb-4"
-          id="password"
-          placeholder="Enter your new password"
-          name="password"
-          type="password"
-          value={data.password}
-          onChange={(e) =>
-            setData({
-              ...data,
-              password: e.target.value,
-            })
-          }
-        ></InputBox>
-        <br/>
-        <InputBox 
-          className="bg-white text-gray-900 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 w-full mb-4"
-          id="conf_pwd"
-          placeholder="Enter confirm password"
-          name="confirm"
-          type="password"
-          value={data.conf_pwd}
-          onChange={(e) => setData({ ...data, conf_pwd: e.target.value })}
-        />
-        <br/>
-        <button className="bg-black hover:bg-black text-white font-bold py-2 px-6 w-full rounded focus:outline-none focus:shadow-outline text-xs" type="submit">
-          Change Password
-        </button>
-      </form>
-      </div>
-    </div>           
+    <form className="flex flex-col items-center text-sm w-full md:w-[300px] mb-4 md:mb-[100px]" onSubmit={handlePasswordReset}>
+      <InputBox
+        class="bg-white text-gray-900 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 w-full mb-4"
+        id="password"
+        placeholder="Enter your new password"
+        name="password"
+        type="password"
+        value={data.password}
+        onChange={(e) =>
+          setData({
+            ...data,
+            password: e.target.value,
+          })
+        }
+      ></InputBox>
+      <br/>
+      <InputBox 
+        class="bg-white text-gray-900 focus:outline-none focus:shadow-outline border border-gray-300 rounded py-2 px-4 w-full mb-4"
+        id="conf_pwd"
+        placeholder="Enter confirm password"
+        name="confirm"
+        type="password"
+        value={data.conf_pwd}
+        onChange={(e) => setData({ ...data, conf_pwd: e.target.value })}
+      />
+      <br/>
+      <button className="bg-black hover:bg-black text-white font-bold py-2 px-6 w-full rounded focus:outline-none focus:shadow-outline text-xs" type="submit">
+        Change Password
+      </button>
+    </form>
+  </div>
+</div>
+           
   
   );
 }
