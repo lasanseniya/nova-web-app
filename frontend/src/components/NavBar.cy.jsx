95e999cf57b1936bc8ffec2f01d76d6d5d0ff1df
lasanseniya <lasanseniya@gmail.com>
2024-04-01 10:49:27 +0530

    Revert "added navbar component test"
    This reverts commit f71988f22927a3262739dae7b6b76060a1a461f8.

--- frontend/src/components/NavBar.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import NavBar from './NavBar'
-
-describe('<NavBar />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<NavBar />)
-  })
-})
--- frontend/src/components/NewNote.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import NewNote from './NewNote'
-
-describe('<NewNote />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<NewNote />)
-  })
-})
